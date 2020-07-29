8ac4228fa3ca649eba87aa7c40b961920a9f5146
CoolRwh <1016681491@qq.com>
2020-07-29 22:28:43 +0800

    管理后台-商品列表

---
+++ app/Admin/Controllers/ProductsController.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\Admin\Controllers;
+
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class ProductsController extends Controller
+{
+
+    public function index(Content $content)
+    {
+        return $content
+            ->header('商品列表')
+            ->body($this->grid());
+    }
+
+    protected function grid()
+    {
+        $grid = new Grid(new Product);
+
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
+
+        $grid->actions(function ($actions) {
+            $actions->disableView();
+            $actions->disableDelete();
+        });
+        $grid->tools(function ($tools) {
+            // 禁用批量删除按钮
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+
+        return $grid;
+    }
+}
